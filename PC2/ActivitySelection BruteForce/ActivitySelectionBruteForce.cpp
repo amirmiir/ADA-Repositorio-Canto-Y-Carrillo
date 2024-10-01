78fb7381922558d12b9ced4b37c541162780856d
amirmiir <146664121+amirmiir@users.noreply.github.com>
2024-10-01 09:56:42 -0500

    Not a modification update
    There is a chance the repository may have been outdated due to a mistaken data update. So just as a precautionary measure, this update is performed.

--- PC2/ActivitySelection BruteForce/ActivitySelectionBruteForce.cpp
+++ PC2/ActivitySelection BruteForce/ActivitySelectionBruteForce.cpp
@@ -1,58 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*  Estructura de la actividad */
+/* Estructura de la actividad */
 struct Activity {
     int start, end;
 };
 
-bool activityCompare(Activity s1, Activity s2)
-{
-    return (s1.end < s2.end);
-}
+/* Funcion para verificar si el subconjunto de actividades es valido */
+bool isValidSet(const vector<Activity>& activities, const vector<int>& subset) {
+    for (size_t i = 0; i < subset.size(); ++i) {
+        for (size_t j = i + 1; j < subset.size(); ++j) {
 
-/*
-    Función que selecciona un conjunto máximo de actividades que pueden ser ejecutadas,
-    dado que solo una actividad puede ejecutarse a la vez en todo momento.
-*/ 
-void activitySelectionBacktracking(int n, vector<Activity>& actvty) {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    for (auto x : actvty) {
-        // Insertar actividades en la cola de prioridad con fin y comienzo
-        pq.push({x.end, x.start});
-    }
-
-    cout << "Las actividades se eligen en el siguiente orden: ";
-
-    // Seleccionamos la primera actividad
-    auto current = pq.top();
-    auto start = current.second;
-    auto end = current.first;
-    pq.pop();
-
-    cout << "{" << start << "," << end << "} ";
-
-    // Seleccionamos del resto de las actividades
-    while (!pq.empty()) {
-        current = pq.top();
-        pq.pop();
-
-        /*Seleccionamos actividad si su inicio es mayor al final de la anterior*/
-        if (current.second >= end) {
-            start = current.second;
-            end = current.first;
-            cout << "{" << start << "," << end << "} ";
+            if (activities[subset[i]].end > activities[subset[j]].start || activities[subset[j]].end > activities[subset[i]].start) {
+                return false;
+            }
         }
     }
-    cout << endl;
+    return true;
 }
 
+/* Algoritmo de fuerza bruta para seleccionar el subconjunto maximo de actividades */
+void bruteForceActivitySelection(const vector<Activity>& activities) {
+    int n = activities.size();
+    int maxActivities = 0;
+    vector<int> bestSet;
+
+    /* Generar todas las combinaciones posibles de actividades */
+    for (int i = 0; i < (1 << n); ++i) { /* 2^n posibles subconjuntos */
+        vector<int> subset;
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j)) { /* Incluir la actividad j en el subconjunto */
+                subset.push_back(j);
+            }
+        }
+
+        /* Verificar si este subconjunto es valido (no tiene superposicion) */
+        if (isValidSet(activities, subset) && subset.size() > maxActivities) {
+            maxActivities = subset.size();
+            bestSet = subset;
+        }
+    }
+
+    /* Imprimir el mejor conjunto de actividades */
+    cout << "Las actividades seleccionadas en el mejor subconjunto son: \n";
+    for (int i : bestSet) {
+        cout << "{" << activities[i].start << ", " << activities[i].end << "} ";
+    }
+    cout << "\nNúmero máximo de actividades: " << maxActivities << endl;
+}
+
+/* Generar actividades aleatorias */
 void generateRandomActivities(vector<Activity>& arr, int n) {
     srand(time(0)); 
     for (int i = 0; i < n; i++) {
-        int start = rand() % (n + 1);  /*Valor entre 0 y n*/ 
-        int finish = start + (rand() % (n / 2 + 1));  /*Valor entre start y n*/
+        int start = rand() % (n + 1);  /* Valor entre 0 y n */
+        int finish = start + (rand() % (n / 2 + 1));  /* Valor entre start y n */
         arr.push_back({start, finish});
     }
 }
@@ -70,13 +72,13 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << "(" << activities[i].start << ", " << activities[i].end << ")\n";
     }
-    
+
     clock_t start = clock();
-    activitySelectionBacktracking(n, activities);
+    bruteForceActivitySelection(activities);
     clock_t end = clock();
-    
-    double elapsedTime = double(end - start)/CLOCKS_PER_SEC;
-    
+
+    double elapsedTime = double(end - start) / CLOCKS_PER_SEC;
+
     cout << "El algoritmo demoró " << elapsedTime << " segundos en ejecutarse." << endl;
 
     return 0;
